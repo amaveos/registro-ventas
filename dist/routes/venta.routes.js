b311b030375fd5e3662e39d0d95957bfdf66ecaf
Ana Velez Ossa <50458708+amaveos@users.noreply.github.com>
2025-03-02 17:21:49 -0500

    Primer commit - Subiendo el proyecto

--- dist/routes/venta.routes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const venta_controller_1 = require("../controllers/venta.controller");
-const router = (0, express_1.Router)();
-router.post("/ventas", venta_controller_1.registrarVenta);
-router.get("/ventas", venta_controller_1.obtenerInformeVentas);
-exports.default = router;
